ca3ba725505fc82ee8a9eea3b2a896d5afa4a93a
Sagar10041 <Sagar10041@gmail.com>
2023-06-09 11:40:44 +0545

    add birthday

--- 01-birthday-reminder/final/src/App.js
+++ 01-birthday-reminder/final/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import data from './data'
 import List from './List'
+
+
 function App() {
   const [people, setPeople] = useState(data)
   return (
---
+++ 01-birthday-reminder/final/src/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Nav = () => {
+  return (
+    <>
+    <ul>
+        <li>Home</li>
+        <li>About</li>
+    </ul>
+    
+    </>
+  )
+}
